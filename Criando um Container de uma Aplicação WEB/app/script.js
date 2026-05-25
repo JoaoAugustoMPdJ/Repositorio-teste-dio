095312ae396ef882cc4ead06e1be11a426c0cb4b
JoaoAugustoMPdJ <103338792+JoaoAugustoMPdJ@users.noreply.github.com>
2026-05-25 14:23:32 +0000

    dock

---
+++ Criando um Container de uma Aplicação WEB/app/script.js
@@ -0,0 +1,7 @@
+function alterarMensagem() {
+  const mensagem = document.getElementById("mensagem");
+
+  mensagem.textContent = "JavaScript executado com sucesso dentro da aplicação em container!";
+  mensagem.style.fontWeight = "bold";
+  mensagem.style.color = "#1e3c72";
+}
